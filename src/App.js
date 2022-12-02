c6a24144c84518a4a007c1eb6af6db703a76e4ac
MarceloHcb <marcelohcb@msn.com>
2022-12-01 22:36:43 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      savedCards: [],
     };
     this.state = {
       ...INITIAL_STATE,
@@ -33,8 +34,31 @@ class App extends React.Component {
     }, this.validationButton);
   }
 
-  onSaveButtonClick() {
-
+  onSaveButtonClick(event) {
+    event.preventDefault();
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo } = this.state;
+    const newElement = { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo };
+    this.setState(({ savedCards }) => ({
+      savedCards: [...savedCards, newElement],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true,
+    }));
   }
 
   validationButton = () => {
